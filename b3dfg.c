4d508f2e8c1a5df1bacd1d2a9f0e73a0639f236d
Daniel Drake <ddrake@brontes3d.com>
2008-03-13 14:58:10 -0400

    use old b3dfg skeleton
    Can't figure out why no dev file appears in sysfs, but this old version
    seems to work

--- b3dfg.c
+++ b3dfg.c
@@ -1,189 +1,157 @@
 /*
- * Brontes PCI frame grabber driver
+ * b3dfg: Brontes Frame Grabber driver
  *
- * Copyright (C) 2008 3M Company
- * Contact: Daniel Drake <ddrake@brontes3d.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Private software; not distributable
  */
 
-#include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
 
-#define DRVNAME			"b3dfg"
-#define PREFIX			DRVNAME ":"
-#define B3DFG_MAX_MINORS	4
-
-static struct class *b3dfg_class;
+#define DRIVER_NAME "b3dfg"
+#define B3DFG_MAX_DEVS 4
 
 struct b3dfg_dev {
-	struct cdev chardev;
-	struct class_device *classdev;
+    struct cdev chardev;
+    struct class_device *classdev;
 };
 
-struct b3dfg_dev *b3dfg_devs[B3DFG_MAX_MINORS];
-static int b3dfg_major;
+static struct class *b3dfg_class;
+static dev_t b3dfg_devt;
 
-static int b3dfg_open(struct inode *inode, struct file *file)
+static const struct pci_device_id pci_ids[] __devinitdata = {
+	{ PCI_DEVICE(0x1901, 0x0001) },
+	{ },
+};
+
+static int b3dfg_open(struct inode *inode, struct file *filp)
 {
-	struct b3dfg_dev *b3d_dev = container_of(inode->i_cdev,
-		struct b3dfg_dev, chardev);
-	file->private_data = b3d_dev;
+	struct b3dfg_dev *fgdev =
+		container_of(inode->i_cdev, struct b3dfg_dev, chardev);
+
+	filp->private_data = fgdev;
 	return 0;
 }
 
-static int b3dfg_release(struct inode *inode, struct file *file)
+static int b3dfg_release(struct inode *inode, struct file *filp)
 {
 	return 0;
 }
 
-struct file_operations b3dfg_fops = {
+static struct file_operations b3dfg_fops = {
 	.owner = THIS_MODULE,
 	.open = b3dfg_open,
 	.release = b3dfg_release,
 };
 
-static unsigned int __devinit b3dfg_get_next_minor(void)
-{
-	unsigned int i;
-	for (i = 0; i < B3DFG_MAX_MINORS; i++)
-		if (!b3dfg_devs[i])
-			break;
-
-	return i;
-}
-
 static int __devinit b3dfg_probe(struct pci_dev *pdev,
 	const struct pci_device_id *id)
 {
-	int r;
-	unsigned int minor;
-	struct b3dfg_dev *b3d_dev;
+	struct b3dfg_dev *fgdev = kzalloc(sizeof(*fgdev), GFP_KERNEL);
+	int r = 0;
+
+	/* FIXME: always using minor 0, hope we don't probe twice! */
+	int minor = 0;
+	dev_t devno = MKDEV(MAJOR(b3dfg_devt), minor);
+
+	if (fgdev == NULL)
+		return -ENOMEM;
+
+	printk(KERN_INFO DRIVER_NAME ": probe %s\n", pci_name(pdev));
+
+	cdev_init(&fgdev->chardev, &b3dfg_fops);
+	fgdev->chardev.owner = THIS_MODULE;
+
+	r = cdev_add(&fgdev->chardev, devno, 1);
+	if (r)
+		goto err1;
+
+	fgdev->classdev = class_device_create(b3dfg_class, NULL, devno, &pdev->dev,
+		DRIVER_NAME "%d", minor);
+	if (IS_ERR(fgdev->classdev)) {
+		r = PTR_ERR(fgdev->classdev);
+		goto err2;
+	}
 
 	r = pci_enable_device(pdev);
-	if (r < 0)
-		return r;
+	if (r)
+		goto err3;
 
-	minor = b3dfg_get_next_minor();
-	if (minor == B3DFG_MAX_MINORS) {
-		printk(KERN_ERR PREFIX "too many devices\n");
-		r = -EIO;
-		goto err_disable;
-	}
-
-	b3d_dev = kzalloc(sizeof(*b3d_dev), GFP_KERNEL);
-	if (!b3d_dev)
-		goto err_disable;
-
-	cdev_init(&b3d_dev->chardev, &b3dfg_fops);
-	b3d_dev->chardev.owner = THIS_MODULE;
-
-	r = cdev_add(&b3d_dev->chardev, MKDEV(b3dfg_major, minor), 1);
-	if (r < 0)
-		goto err_free_dev;
-
-	b3d_dev->classdev = class_device_create(b3dfg_class, NULL,
-		MKDEV(b3dfg_major, minor), &pdev->dev, DRVNAME "%u", minor);
-	if (IS_ERR(b3d_dev->classdev)) {
-		r = PTR_ERR(b3d_dev->classdev);
-		goto err_cdev_del;
-	}
-
-	b3dfg_devs[minor] = b3d_dev;
-	pci_set_drvdata(pdev, b3d_dev);
-	printk(KERN_INFO PREFIX "adding device %u\n", minor);
+	pci_set_drvdata(pdev, fgdev);
 	return 0;
 
-err_cdev_del:
-	cdev_del(&b3d_dev->chardev);
-err_free_dev:
-	kfree(b3d_dev);
-err_disable:
-	pci_disable_device(pdev);
+err3:
+	class_device_unregister(fgdev->classdev);
+err2:
+	cdev_del(&fgdev->chardev);
+err1:
+	kfree(fgdev);
 	return r;
 }
 
 static void __devexit b3dfg_remove(struct pci_dev *pdev)
 {
-	struct b3dfg_dev *b3d_dev = pci_get_drvdata(pdev);
-	unsigned int minor = MINOR(b3d_dev->chardev.dev);
+	struct b3dfg_dev *fgdev = pci_get_drvdata(pdev);
 
-	class_device_unregister(b3d_dev->classdev);
-	cdev_del(&b3d_dev->chardev);
-	b3dfg_devs[minor] = NULL;
-	kfree(b3d_dev);
+	printk(KERN_INFO DRIVER_NAME ": remove\n");
+
 	pci_disable_device(pdev);
-	printk(KERN_INFO PREFIX "removing device %u\n", minor);
+	class_device_unregister(fgdev->classdev);
+	cdev_del(&fgdev->chardev);
+
+	kfree(fgdev);
 }
 
-static struct pci_device_id b3dfg_ids[] __devinitdata = {
-	{ PCI_DEVICE(0x1901, 0x0001) },
-	{ 0, },
-};
-
 static struct pci_driver b3dfg_driver = {
-	.name = DRVNAME,
-	.id_table = b3dfg_ids,
+	.name = DRIVER_NAME,
+	.id_table = pci_ids,
 	.probe = b3dfg_probe,
 	.remove = b3dfg_remove,
 };
 
-static int __init b3dfg_module_init(void)
+static int __init b3dfg_init(void)
 {
-	dev_t first_dev;
 	int r;
 
-	r = alloc_chrdev_region(&first_dev, 0, B3DFG_MAX_MINORS, DRVNAME);
-	if (r < 0)
-		return r;
+	printk(KERN_INFO DRIVER_NAME " loaded\n");
 
-	b3dfg_class = class_create(THIS_MODULE, DRVNAME);
-	if (IS_ERR(b3dfg_class)) {
-		r = PTR_ERR(b3dfg_class);
-		goto out_chrdev;
-	}
+	b3dfg_class = class_create(THIS_MODULE, DRIVER_NAME);
+	if (IS_ERR(b3dfg_class))
+		return PTR_ERR(b3dfg_class);
+
+	r = alloc_chrdev_region(&b3dfg_devt, 0, B3DFG_MAX_DEVS, DRIVER_NAME);
+	if (r)
+		goto err1;
 
 	r = pci_register_driver(&b3dfg_driver);
-	if (r < 0)
-		goto out_class;
+	if (r)
+		goto err2;
 
-	b3dfg_major = MAJOR(first_dev);
-	printk(KERN_INFO PREFIX "loaded\n");
-	return 0;
-out_class:
+	return r;
+
+err2:
+	unregister_chrdev_region(b3dfg_devt, B3DFG_MAX_DEVS);
+err1:
 	class_destroy(b3dfg_class);
-out_chrdev:
-	unregister_chrdev_region(first_dev, B3DFG_MAX_MINORS);
 	return r;
 }
 
-static void __exit b3dfg_module_exit(void)
+static void __exit b3dfg_exit(void)
 {
 	pci_unregister_driver(&b3dfg_driver);
-	unregister_chrdev_region(MKDEV(b3dfg_major, 0), B3DFG_MAX_MINORS);
+	unregister_chrdev_region(b3dfg_devt, B3DFG_MAX_DEVS);
 	class_destroy(b3dfg_class);
-	printk(KERN_INFO PREFIX "unloaded\n");
 }
 
+module_init(b3dfg_init);
+module_exit(b3dfg_exit);
+
 MODULE_AUTHOR("Daniel Drake <ddrake@brontes3d.com>");
-MODULE_DESCRIPTION("Brontes PCI frame grabber driver");
+MODULE_DESCRIPTION("Brontes frame grabber driver");
 MODULE_LICENSE("GPL");
-MODULE_DEVICE_TABLE(pci, b3dfg_ids);
-module_init(b3dfg_module_init);
-module_exit(b3dfg_module_exit);
+MODULE_DEVICE_TABLE(pci, pci_ids);
 
