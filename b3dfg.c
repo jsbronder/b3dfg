e1e448dd703bbd440b0b8a141f7fcd0591bc466d
Daniel Drake <ddrake@brontes3d.com>
2008-04-21 15:56:01 -0400

    dont ack all-zero interrupts

--- b3dfg.c
+++ b3dfg.c
@@ -510,6 +510,8 @@ static irqreturn_t handle_interrupt(struct b3dfg_dev *fgdev)
 	sts = b3dfg_read32(fgdev, B3D_REG_DMA_STS);
 	if (unlikely(sts == 0)) {
 		printk("ignore interrupt, brontes DMA status is 0\n");
+		/* temporarily dont ack, req by jerry 20080418 */
+		need_ack = 0;
 		/* FIXME should return IRQ_NONE when we are stable */
 		goto out;
 	}
