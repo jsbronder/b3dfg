jsbronder/b3dfg

commit 325b61fbd9423d359df0a821ecaeb55c7d7fb88a

Buffers will be automatically dequeued when tx is disabled

b3dfg.c
@@ -791,9 +791,6 @@ static int b3dfg_release(struct inode *inode, struct file *filp)
 	struct b3dfg_dev *fgdev = filp->private_data;
 	dev_dbg(&fgdev->pdev->dev, "release\n");
 	set_transmission(fgdev, 0);
-
-	/* no buffer locking needed, this is serialized */
-	dequeue_all_buffers(fgdev);
 	return 0;
 }
 

commit b57b0d41dc4ce4dacb0da70dd8918b885ca868a3

Unconditionally disable transmission when releasing device.
This fixes a regression introduced in commit
325b61fbd9423d359df0a821ecaeb55c7d7fb88a which causes queued buffers to
not be unqueued when releasing a device for which transmission was not
enabled.

b3dfg.c
@@ -788,7 +788,7 @@ static int b3dfg_release(struct inode *inode, struct file *filp)
 {
 	struct b3dfg_dev *fgdev = filp->private_data;
 	dev_dbg(&fgdev->pdev->dev, "release\n");
-	set_transmission(fgdev, 0);
+	disable_transmission(fgdev);
 	return 0;
 }
 