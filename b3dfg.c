2cd7fa233da365eaa853ebfef3e44b178ea93684
Justin Bronder <jsbronder@gmail.com>
2010-07-22 17:57:38 -0400

    Revert "Replace all DMA_nBIT_MASK macro with DMA_BIT_MASK(n)"
    This reverts commit c5a81ba9edc007a4a1d8faa6c55a36fd9374c050.
    
    Applied incorrectly

--- b3dfg.c
+++ b3dfg.c
@@ -992,7 +992,7 @@ static int __devinit b3dfg_probe(struct pci_dev *pdev,
 
 	pci_set_master(pdev);
 
-	r = pci_set_dma_mask(pdev, DMA_BIT_MASK(32));
+	r = pci_set_dma_mask(pdev, DMA_32BIT_MASK);
 	if (r) {
 		dev_err(&pdev->dev, "no usable DMA configuration\n");
 		goto err_free_res;
