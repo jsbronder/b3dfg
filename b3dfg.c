74cec18f6b6657980a635a13658d642f8ee4a07f
Vasilis Liaskovitis <vliaskov@gmail.com>
2010-07-22 18:15:39 -0400

    Staging: b3dfg: remove check for pci bus master
    Remove unneccesary check for pci master in enable_transmission()
    
    Signed-off-by: Vasilis Liaskovitis <vliaskov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- b3dfg.c
+++ b3dfg.c
@@ -480,17 +480,6 @@ static int enable_transmission(struct b3dfg_dev *fgdev)
 		return -EINVAL;
 	}
 
-	/*
-	 * Check we're a bus master.
-	 * TODO: I think we can remove this having added the pci_set_master call
-	 */
-	pci_read_config_word(fgdev->pdev, PCI_COMMAND, &command);
-	if (!(command & PCI_COMMAND_MASTER)) {
-		dev_err(dev, "not a bus master, force-enabling\n");
-		pci_write_config_word(fgdev->pdev, PCI_COMMAND,
-			command | PCI_COMMAND_MASTER);
-	}
-
 	spin_lock_irqsave(&fgdev->buffer_lock, flags);
 
 	/* Handle racing enable_transmission calls. */
