f8d23b44b23ff90b6f7e7255b3fb0bd95e671f33
Justin Bronder <jsbronder@gmail.com>
2010-09-03 15:53:04 -0400

    libb3dfg:  add cable state example
    Small example that just prints the current cable status and exits.
    Useful for testing that we can still talk to the driver while another
    application is locking the buffers.

---
+++ libb3dfg/examples/cstate.c
@@ -0,0 +1,36 @@
+/*
+ * Brontes frame grabber example
+ *
+ * Copyright (C) 2008 3M Company
+ * Author: Daniel Drake <ddrake@brontes3d.com>
+ *
+ * Private software. Source code may not be distributed.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <libb3dfg/b3dfg.h>
+
+
+int main(void)
+{
+    static b3dfg_dev *dev;
+    int r = 0;
+
+	dev = b3dfg_open(0);
+	if (!dev) {
+		fprintf(stderr, "open failed\n");
+        r = 1;
+		goto out;
+	}
+
+	printf("wand is %s\n",
+		b3dfg_get_wand_status(dev) ? "connected" : "disconnected");
+
+	b3dfg_close(dev);
+out:
+	return r;
+}
